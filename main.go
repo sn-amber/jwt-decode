71f400fd66ffcf2a0a2722c7adaa0b95025c0f4f
Stefan Amberger <amberger.stefan@gmail.com>
2017-04-07 13:57:48 +0200

    refactor

--- main.go
+++ main.go
@@ -6,34 +6,76 @@ import (
 	"encoding/base64"
 	"strings"
 	"encoding/json"
+	"fmt"
+	"log"
+	"flag"
+)
+
+var (
+	token string
 )
 
 func main() {
-	input, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		panic(err)
+
+	flag.StringVar(&token, "token", "", "token to be decoded")
+	flag.Parse()
+
+	if token == "" {
+		var err error
+		token, err = readTokenFromStdin()
+		fatalOnErr(err, "cannot read token from stdin")
 	}
 
-	segments := strings.Split(string(input), ".")
-
-	enc := json.NewEncoder(os.Stdout)
-	enc.SetIndent("", "    ")
+	// split token into three parts
+	segments := strings.Split(token, ".")
 
 	for _, segment := range segments[:len(segments)-1] {
-		decoded, err := base64.RawStdEncoding.DecodeString(segment)
-		if err != nil {
-			panic(err)
-		}
 
-		var parsed interface{}
-		err = json.Unmarshal(decoded, &parsed)
-		if err != nil {
-			panic(err)
-		}
+		data, err := base64ToJson(segment)
+		fatalOnErr(err, "cannot decode from base64")
 
-		err = enc.Encode(parsed)
-		if err != nil {
-			panic(err)
-		}
+		err = prettyPrintJson(data)
+		fatalOnErr(err, "cannot pretty-print json")
+	}
+}
+
+func readTokenFromStdin() (string, error) {
+	bt, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		return "", err
+	}
+	return string(bt), nil
+}
+
+func base64ToJson(str string) (interface{}, error) {
+
+	// decode
+	decoded, err := base64.RawStdEncoding.DecodeString(str)
+	if err != nil {
+		return "", err
+	}
+
+	// unmarshal
+	var parsed interface{}
+	err = json.Unmarshal(decoded, &parsed)
+	if err != nil {
+		return "", err
+	}
+	return parsed, nil
+}
+
+func prettyPrintJson(data interface{}) error {
+	bt, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(bt))
+	return nil
+}
+
+func fatalOnErr(err error, desc string) {
+	if err != nil {
+		log.Fatalln(desc, err)
 	}
 }
